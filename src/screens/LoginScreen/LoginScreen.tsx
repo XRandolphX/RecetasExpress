737e9e43ef2e0953e029d154c3ae8bff16b9ee8b
XRandolphX <randolphfrp@gmail.com>
2025-05-19 09:02:40 -0500

    feat: enhance LoginScreen with login functionality and input handling

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -3,6 +3,24 @@ import { View, Text, TextInput, Button, Alert, StyleSheet } from "react-native";
 import { useAuth } from "../../context/AuthContext";
 
 export default function LoginScreen() {
-  const login = useAuth();
-  return("");
+  const { login } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (email === "test@demo.com" && password === "123456") {
+      login();
+    } else {
+      Alert.alert("Error", "Usuario o contraseña incorrectos");
+    }
+  };
+  return (
+    <View>
+      <Text>Iniciar Sesión</Text>
+      <TextInput />
+      <TextInput />
+      <Button title="Iniciar Sesión" onPress={handleLogin} />
+    </View>
+  );
 }
